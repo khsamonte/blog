8e8e10f31b60ccf7464ad22d3b384e52b4f08d58
Ken Samonte <khsamonte@gmail.com>
2024-05-01 00:24:42 +0800

    Added GA tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga";
 
 // Components
 import MainLayout from "./components/MainLayout";
@@ -9,39 +10,51 @@ import Post from "./components/Post";
 // Pages
 import HomePage from "./routes/HomePage";
 
+// Tracking ID
+const TRACKING_ID = "G-BTHZPV49BG";
+
+function usePageTracking() {
+	useEffect(() => {
+		ReactGA.initialize(TRACKING_ID);
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, []);
+}
+
 function App() {
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <MainLayout>
-              <HomePage />
-            </MainLayout>
-          }
-        />
+	usePageTracking();
 
-        <Route
-          path="/:category"
-          element={
-            <MainLayout>
-              <Category />
-            </MainLayout>
-          }
-        />
+	return (
+		<Router basename={process.env.PUBLIC_URL}>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<MainLayout>
+							<HomePage />
+						</MainLayout>
+					}
+				/>
 
-        <Route
-          path="/:category/:post"
-          element={
-            <MainLayout>
-              <Post />
-            </MainLayout>
-          }
-        />
-      </Routes>
-    </Router>
-  );
+				<Route
+					path="/:category"
+					element={
+						<MainLayout>
+							<Category />
+						</MainLayout>
+					}
+				/>
+
+				<Route
+					path="/:category/:post"
+					element={
+						<MainLayout>
+							<Post />
+						</MainLayout>
+					}
+				/>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
